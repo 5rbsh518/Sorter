68acf1c4da8214294ac606d6a880af3f0c756e23
5rbsh518 <Omerrr350@hotmail.com>
2023-02-11 21:56:01 +0300

    change sort method

--- Sorter.java
+++ Sorter.java
@@ -13,8 +13,6 @@ public class Sorter {
             arr[i] = smallerNumber;  
         }  
     }  
-
-    
     public static void insertionSort(int array[]) {  
         int n = array.length;  
         for (int j = 1; j < n; j++) {  
@@ -27,9 +25,9 @@ public class Sorter {
             array[i+1] = key;  
         }  
     } 
-
     public static void sort(int[] array){
-        insertionSort(array);
+        selectionSort(array);
+
     }
 }
 
